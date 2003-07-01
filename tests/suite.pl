c6818e27551a117f96fd523db55bdf443cfba091
jaredy <>
2003-07-01 12:55:38 +0000

    beautify, added color output, more tests

--- tests/suite.pl
+++ tests/suite.pl
@@ -17,7 +17,7 @@ sub fail()
 
 sub princ
 {
-	print "\033[1;0;34m", @_, ":\033[1;0;0m\n";
+	print "\033[1;40;34m", @_, ":\033[1;0;0m\n";
 }
 
 my $w = WASP->new;
@@ -25,11 +25,13 @@ $w->die(1);
 #my $d = DBH->new();
 my $t = Thraxx->construct(wasp=>$w, dbh=>1, skip_init=>1);
 
+my ($s, $p, $i, $j, $k);
+
 ########################################################################
 # crypt.inc
 princ "Random alphanumeric characters";
 eval {
-print $t->rand_str(25, Thraxx::RAND_VIS_ALNUM), "\n"	for 1 .. 3; 1
+print $t->rand_str(25, Thraxx::RAND_VIS_ALNUM), "\n"	for 1 .. 3;
 }; $@ ? fail : ok;
 
 princ "Random non-quote characters";
@@ -80,29 +82,24 @@ princ "in_array()";
 print "5 in (", 2..7, ") and 1 not in (", 2..7 ,")\n";
 Thraxx::in_array(5, [2 .. 7]) && !Thraxx::in_array(1, [2 .. 7]) ? ok : fail;
 
+princ "in_hash()";
+print "5 in (a=>1, b=>5) and 1 not in (a=>6, b=>2, c=>7)\n";
+Thraxx::in_hash(5, {a=>1, b=>5}) && !Thraxx::in_hash(1, {a=>6, b=>2, c=>7}) ? ok : fail;
+
 princ "slurp_file()";
-
-my $s="this
-is
-my
-favorite string";
-
-{
-my $_t = $s;
-$_t =~ s/\n/\\n/g;
-printf "Writing to file: $_t\n";
-}
+$s="this \nis\n my \nfavorite string";
+$k = $s;
+$k =~ s/\n/\\n/g;
+printf "Writing to file: $k\n";
 
 open OUT, "> out";
 print OUT $s;
 close OUT;
 
-my $p = $t->slurp_file("out");
-{
-my $_t = $p;
-$_t =~ s/\n/\\n/g;
-print "Reading from file: $_t\n";
-}
+$p = $t->slurp_file("out");
+$k = $p;
+$k =~ s/\n/\\n/g;
+print "Reading from file: $k\n";
 $s eq $p ? ok : fail;
 
 princ "write_config()";
@@ -117,6 +114,18 @@ $t->write_config;
 
 #my $uid = $t->user_add(password=>"hi there");
 
+########################################################################
+# str.inc
+
+princ "encode_html()";
+$s = qq!<html>my "string" & is here</html>!;
+$p = $t->encode_html($s);
+$k = qq!&lt;html&gt;my &quot;string&quot; &amp; is here&lt;/html&gt;!;
+print "string: $s\n";
+print "encoded: $p\n";
+$p eq $k ? ok : fail;
+
+# str_parse(), oh boy
 
 ########################################################################
 # sessions.inc
